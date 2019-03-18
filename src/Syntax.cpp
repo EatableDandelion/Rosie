12d8cb6ba3cb849654b35878a977d843e69cb5b1
EatableDandelion <46005846+EatableDandelion@users.noreply.github.com>
2019-03-17 21:32:29 -0500

    Update Syntax.cpp

--- src/Syntax.cpp
+++ src/Syntax.cpp
@@ -2,10 +2,10 @@
 
 namespace Rosie
 {
-	Opcode::Opcode(const std::string& name, const std::function<void(std::vector<Address>&)> func):name(name), func(func)
+	Opcode::Opcode(const std::string& name, const std::function<void(std::vector<Variable>&)> func):name(name), func(func)
 	{}
 	
-	void Opcode::execute(const std::vector<Address>& arguments)
+	void Opcode::execute(const std::vector<Variable>& arguments)
 	{
 		func(arguments);
 	}
@@ -17,17 +17,17 @@ namespace Rosie
 
 	Syntax::Syntax()
 	{	
-		addOpcode("ADD", [](std::vector<Address>& args){args(0).setValue(args(1).getValue()+args(2).getValue());});
+		addOpcode("ADD", [](std::vector<Variable>& args){args(0).setValue(args(1).getValue()+args(2).getValue());});
 	}
 	//TODO add the opcodes here
 	//TODO add the value to the address directly: setValue and getValue
 	
-	void Syntax::addOpcode(const std::string& name, const std::function<void(std::vector<Address>&)> func)
+	void Syntax::addOpcode(const std::string& name, const std::function<void(std::vector<Variable>&)> func)
 	{
 		opcodes[name] = Opcode(name, func);
 	}
 	
-	void Syntax::execute(const std::string& name, const std::vector<Address>& arguments)
+	void Syntax::execute(const std::string& name, const std::vector<Variable>& arguments)
 	{
 		opcodes[name].execute(arguments);
 	}

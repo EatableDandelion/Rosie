d2295b5c89ef32692f9b9d9b1f126c025c95e93f
EatableDandelion <46005846+EatableDandelion@users.noreply.github.com>
2019-03-17 21:28:23 -0500

    Update Syntax.h

--- include/Syntax.h
+++ include/Syntax.h
@@ -10,14 +10,14 @@ namespace Rosie
 	struct Opcode
 	{
 		public:
-			Opcode(const std::string& name, const std::function<void(std::vector<Address>&)> func);
+			Opcode(const std::string& name, const std::function<void(std::vector<Variable>&)> func);
 		
-			void execute(const std::vector<Address>& arguments);
+			void execute(const std::vector<Variable>& arguments);
 			std::string getName() const;
 		
 		private:
 			std::string name;
-			std::function<void(std::vector<Address>&)> func;
+			std::function<void(std::vector<Variable>&)> func;
 	};
 	
 	class Syntax
@@ -25,9 +25,9 @@ namespace Rosie
 		public:
 			Syntax();
 			
-			void addOpcode(const std::string& name, const std::function<void(std::vector<Address>&)> func);
+			void addOpcode(const std::string& name, const std::function<void(std::vector<Variable>&)> func);
 
-			void execute(const std::string& name, const std::vector<Address>& arguments);
+			void execute(const std::string& name, const std::vector<Variable>& arguments);
 			
 		private:
 			std::unordered_map<std::string, Opcode> opcodes;

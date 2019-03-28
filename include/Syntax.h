b9dc43b8d65d60616915c2189b9d26cb5555ee4c
EatableDandelion <46005846+EatableDandelion@users.noreply.github.com>
2019-03-28 15:19:32 -0500

    Added DualMap

--- include/Syntax.h
+++ include/Syntax.h
@@ -42,6 +42,59 @@ namespace Rosie
 			std::function<void(std::vector<T>&)> m_func;
 	};
 	
+	template<typename K1, typename K2, typename V>
+	class DualMap //map that can be searched for either K1 OR K2
+	{
+		public:
+			void add(const K1&, key1 const K2& key2, const V& value)
+			{
+				values.insert(std::pair<K2,V>(key2, value));
+				keymap.insert(std::pair<K1,K2>(key1, key2));
+			}
+		
+			bool contains(const K1& key1)
+			{
+				return keymap.find(key1) != keymap.end();
+			}
+		
+			bool contains(const K2& key2)
+			{
+				return values.find(key2) != values.end();
+			}
+		
+			V operator[](const K1& key1) const
+			{
+				return values[keymap[key1]];
+			}
+		
+			V operator[](const K2& key2) const
+			{
+				return values[key2];
+			}
+		
+			void erase(const K1& key1)
+			{
+				values.erase(keymap[key1]);
+				keymap.erase(key1);
+			}
+		
+			void erase(const K2& key2)
+			{
+				values.erase(key2);
+				for(const auto& it : keymap)
+				{
+					if(it.second == key2)
+					{
+						keymap.erase(it.first);
+					}
+				}
+			}
+		
+		private:
+			std::unordered_map<K2, V> values;
+			std::unordered_map<K1, K2> keymap;
+	};
+	
 	class Syntax
 	{
 		public:
@@ -64,7 +117,7 @@ namespace Rosie
 			std::unordered_map<std::string, Function<Variable>> getNativeMethods() const;
 			
 		private:
-			std::unordered_map<std::string, Function<int>> opcodes;
+			DualMap<int, std::string, Function<int>> opcodes;
 			std::unordered_map<std::string, Function<Variable>> methods;
 			std::unordered_map<int, Variable> variables;
 			std::unordered_map<int, Variable> constants;

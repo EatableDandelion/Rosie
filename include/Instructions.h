40fe31cd710a3d70b09804481761d49b2e0a11bb
EatableDandelion <46005846+EatableDandelion@users.noreply.github.com>
2019-04-09 13:21:34 -0500

    Update Instructions.h

--- include/Instructions.h
+++ include/Instructions.h
@@ -1,8 +1,32 @@
+#pragma once
+
+#include "VMObjects.h"
+
 namespace Rosie
 {
   template<typename T>
   class Instruction
   {
+    public:
+      template<typename... Args>
+      Instruction(Args&&... args):instruction(std::forward<Args>(args)...)
+      {}
     
+      void runInstruction(const std::string& command, State& state)
+      {
+        instruction.run(command, state);
+      }
+    
+    private:
+      T instruction;
   };
+  
+  class SetInstruction
+  {
+    public:
+      SetInstruction(const int& destId, const int& srcId,  const int& srcType);
+      void run(const std::string& command, State& state) const;
+  };
+  
+  
 }

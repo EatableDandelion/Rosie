85c65456751f848456a8bc44fa2563740dbac28d
EatableDandelion <46005846+EatableDandelion@users.noreply.github.com>
2019-04-09 13:48:14 -0500

    Update Instructions.h

--- include/Instructions.h
+++ include/Instructions.h
@@ -8,23 +8,29 @@ namespace Rosie
   class Instruction
   {
     public:
-      template<typename... Args>
-      Instruction(Args&&... args):instruction(std::forward<Args>(args)...)
+      Instruction(const Opcodes& opcode):opcode(opcode)
       {}
+    
+      template<typename... Args>
+      std::string getString(Args&&... args) const
+      {
+        return opcode+"|"+instruction.getString(std::forward<Args>(args)...);
+      }
     
       void runInstruction(const std::string& command, State& state)
       {
-        instruction.run(command, state);
+        instruction.run(command.substr(line.find("|", command)+1, line.size()), state);
       }
     
     private:
       T instruction;
+      Opcode opcode;
   };
   
   class SetInstruction
   {
     public:
-      SetInstruction(const int& destId, const int& srcId,  const int& srcType);
+      std::string getString(const int& destId, const int& srcId,  const int& srcType) const;
       void run(const std::string& command, State& state) const;
   };
   

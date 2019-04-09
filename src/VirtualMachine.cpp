9eade87c1adac51d4c39c08678d837e7757d6302
EatableDandelion <46005846+EatableDandelion@users.noreply.github.com>
2019-04-09 11:49:44 -0500

    Update VirtualMachine.cpp

--- src/VirtualMachine.cpp
+++ src/VirtualMachine.cpp
@@ -44,7 +44,7 @@ namespace Rosie
 	}
 	
 	
-	Handle VirtualMachine::parseHandle(const std_string& text) const
+	Handle VirtualMachine::parseHandle(const std::string& text) const
 	{
 		std::size_t slashPos = line.find("/", std::size_t(0));
 		std::string addressText = line.substr(std::size_t(0), slashPos-std::size_t(0)));

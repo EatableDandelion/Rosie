eac151c1d41c5492da2c9284292e896ae96ef08e
EatableDandelion <46005846+EatableDandelion@users.noreply.github.com>
2019-07-25 23:07:39 +0200

    Update Token.h

--- include/Token.h
+++ include/Token.h
@@ -27,10 +27,6 @@ namespace Rosie
 		std::string value;
 		int charIndex;
 		
-		Token();
-		
-		Token(const std::string& value, const TokenType& type);
-		
 		void operator+=(const char c);
 		
 		bool operator==(const std::string& stringValue) const;
